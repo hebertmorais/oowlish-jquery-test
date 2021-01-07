c2c8d691f7cdeb2bee0ae3fd918a6953599fe9e0
Hebert Morais <hebertlucashllm@gmail.com>
2021-01-07 10:09:44 -0300

    Added get doctors from api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import "./index.css";
+
+const API_URL = "http://localhost:3030/doctors";
+
+let doctors = [];
+
+const onInit = () => {
+  getDoctors();
+};
+
+const getDoctors = (availability = undefined) => {
+  const availabilityParam = availability
+    ? `/available${availability}`
+    : "";
+  $.get(`${API_URL}${availabilityParam}`, (data) => {
+    doctors = data;
+  });
+};
+
+$(document).ready(() => {
+  onInit();
+});
