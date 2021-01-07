2a53e072ff92aae9c516e3f7453d2fb250505a38
Hebert Morais <hebertlucashllm@gmail.com>
2021-01-07 10:56:18 -0300

    Adding indicator to show doctors availability

--- src/index.js
+++ src/index.js
@@ -6,14 +6,28 @@ let doctors = [];
 
 const onInit = () => {
   getDoctors();
+  addAvailabilityIndicator();
 };
 
 const getDoctors = (availability = undefined) => {
-  const availabilityParam = availability
-    ? `/available${availability}`
-    : "";
-  $.get(`${API_URL}${availabilityParam}`, (data) => {
-    doctors = data;
+  const availabilityParam = availability ? `/available${availability}` : "";
+  $.ajax({
+    method: 'GET',
+    url: `${API_URL}${availabilityParam}`,
+    success: (data) => {
+      console.log(data)
+      doctors = data;
+    },
+    async: false,
+  });
+};
+
+const addAvailabilityIndicator = () => {
+  doctors.forEach((doctor, _) => {
+    if (doctor.available) {
+      let item = $(`[data-upin='${doctor.upin}']`).first();
+      item.addClass("available");
+    }
   });
 };
 
