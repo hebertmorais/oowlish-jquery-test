b6543946fbaa42d2329ab4a90f9d50ecce93abc4
Hebert Morais <hebertlucashllm@gmail.com>
2021-01-07 14:03:08 -0300

    Added button function to set doctor availability

--- src/index.js
+++ src/index.js
@@ -13,10 +13,9 @@ const onInit = () => {
 const getDoctors = (availability = undefined) => {
   const availabilityParam = availability ? `/available${availability}` : "";
   $.ajax({
-    method: 'GET',
+    method: "GET",
     url: `${API_URL}${availabilityParam}`,
     success: (data) => {
-      console.log(data)
       doctors = data;
     },
     async: false,
@@ -25,30 +24,67 @@ const getDoctors = (availability = undefined) => {
 
 const addAvailabilityIndicator = () => {
   doctors.forEach((doctor, _) => {
-    if (doctor.available) {
-      let item = $(`[data-upin='${doctor.upin}']`).first();
-      item.addClass("available");
-    }
+    updateDoctorRowFromUPIN(doctor);
   });
 };
 
 const createListeners = () => {
-  $('#availabilityFilterSelect').on('change', event => {
+  $("#availabilityFilterSelect").on("change", (event) => {
     const filterValue = $(event.currentTarget).val();
     handleFilterChange(filterValue);
   });
-}
+
+  $(".button").on("click", (event) => {
+    const upin = $(event.currentTarget).parent().parent().attr("data-upin");
+    let doctorWithUPIN = doctors.filter((doctor) => {
+      return doctor.upin == upin;
+    })[0];
+
+    doctorWithUPIN.available = !doctorWithUPIN.available;
+
+    changeDoctorAvailability(doctorWithUPIN);
+  });
+};
 
 const handleFilterChange = (value) => {
   doctors.forEach((doctor, _) => {
-    let item = $(`[data-upin='${doctor.upin}']`).first();
+    let item = getDoctorRowFromUPIN(doctor.upin);
     if (value == "available" && !doctor.available) {
       item.addClass("hidden");
     } else if (value == "all") {
       item.removeClass("hidden");
     }
   });
-}
+};
+
+const changeDoctorAvailability = (doctor) => {
+  $.ajax({
+    method: "PUT",
+    contentType: "application/json",
+    url: `${API_URL}/${doctor.upin}`,
+    data: JSON.stringify(doctor),
+    success: (updatedDoctor) => {
+      updateDoctorRowFromUPIN(updatedDoctor);
+    },
+  });
+};
+
+const updateDoctorRowFromUPIN = (doctor) => {
+  const doctorRow = getDoctorRowFromUPIN(doctor.upin);
+  const buttonText = `Mark as ${doctor.available ? "Una" : "A"}vailable`;
+
+  doctorRow.find(".button").html(buttonText);
+
+  if (doctor.available) {
+    doctorRow.addClass("available");
+  } else {
+    if (doctorRow.hasClass("available")) doctorRow.removeClass("available");
+  }
+};
+
+const getDoctorRowFromUPIN = (upin) => {
+  return $(`[data-upin='${upin}']`).first();
+};
 
 $(document).ready(() => {
   onInit();
